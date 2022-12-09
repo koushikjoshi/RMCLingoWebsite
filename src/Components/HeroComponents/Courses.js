c3d916ed5f4e01c77ae6793ee977971e013a6d3a
rishab-suresh <sureshrishab6@gmail.com>
2022-12-09 16:32:22 +0530

    added links in home carousel

--- src/Components/HeroComponents/Courses.js
+++ src/Components/HeroComponents/Courses.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { Link } from "react-router-dom";
 
 import "./Courses.css";
 
@@ -16,7 +17,7 @@ export const Courses = () => {
         centerMode={false}
         className="Courses-carousel"
         containerClass="container-with-dots"
-        dotListClass=""
+        dotListClass="links"
         draggable
         focusOnSelect={true}
         infinite
@@ -57,51 +58,79 @@ export const Courses = () => {
         swipeable
       >
         <div className="Courses-card" id="courses-id">
-          <img
-            src={require("../../Assets/germany.png")}
-            alt="German Language"
-          />
-          <h4 className="Courses-title">German Language</h4>
+          <Link to="/Germany">
+            <img
+              src={require("../../Assets/germany.png")}
+              alt="German Language"
+            />
+            <h4 className="Courses-title">German Language</h4>
+          </Link>
         </div>
         <div className="Courses-card">
-          <img src={require("../../Assets/france.png")} alt="French Language" />
-          <h4 className="Courses-title">French Language</h4>
+          <Link to="/French">
+            <img
+              src={require("../../Assets/france.png")}
+              alt="French Language"
+            />
+            <h4 className="Courses-title">French Language</h4>
+          </Link>
         </div>
         <div className="Courses-card">
-          <img src={require("../../Assets/spain.png")} alt="Spanish Language" />
-          <h4 className="Courses-title">Spanish Language</h4>
+          <Link to="/Spanish">
+            <img
+              src={require("../../Assets/spain.png")}
+              alt="Spanish Language"
+            />
+            <h4 className="Courses-title">Spanish Language</h4>
+          </Link>
         </div>
         <div className="Courses-card">
-          <img
-            src={require("../../Assets/japan.png")}
-            alt="Japanese Language"
-          />
-          <h4 className="Courses-title">Japanese Language</h4>
+          <Link to="Japanese">
+            <img
+              src={require("../../Assets/japan.png")}
+              alt="Japanese Language"
+            />
+            <h4 className="Courses-title">Japanese Language</h4>
+          </Link>
         </div>
         <div className="Courses-card">
-          <img
-            src={require("../../Assets/russia.png")}
-            alt="Russian Language"
-          />
-          <h4 className="Courses-title">Russian Language</h4>
+          <Link to="Russian">
+            <img
+              src={require("../../Assets/russia.png")}
+              alt="Russian Language"
+            />
+            <h4 className="Courses-title">Russian Language</h4>
+          </Link>
         </div>
         <div className="Courses-card">
-          <img src={require("../../Assets/dubai.png")} alt="Arabic Language" />
-          <h4 className="Courses-title">Arabic Language</h4>
+          <Link to="/Arabic">
+            <img
+              src={require("../../Assets/dubai.png")}
+              alt="Arabic Language"
+            />
+            <h4 className="Courses-title">Arabic Language</h4>
+          </Link>
         </div>
         <div className="Courses-card">
-          <img src={require("../../Assets/korea.png")} alt="Korean Language" />
-          <h4 className="Courses-title">Korean Language</h4>
+          <Link to="/Korean">
+            <img
+              src={require("../../Assets/korea.png")}
+              alt="Korean Language"
+            />
+            <h4 className="Courses-title">Korean Language</h4>
+          </Link>
         </div>
         <div className="Courses-card">
-          <img
-            src={require("../../Assets/england.png")}
-            alt="English Language"
-          />
-          <h4 className="Courses-title">English Language</h4>
+          <Link to="/English">
+            <img
+              src={require("../../Assets/england.png")}
+              alt="English Language"
+            />
+            <h4 className="Courses-title">English Language</h4>
+          </Link>
         </div>
       </Carousel>
-      <button className="Courses-button">Learn More</button>
+      <Link to="/Courses"> <button className="Courses-button"> Learn More</button> </Link>
     </div>
   );
 };
